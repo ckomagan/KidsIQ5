347710b0ebcff35bb65e33a30bd8961a5a209f53
Chan Komagan <chan@komagan.com>
2012-09-26 00:06:02 -0400

    Added support for in-app purchase, country list, preliminary leader board

--- KidsIQ5/KidsIQ5ViewController.h
+++ KidsIQ5/KidsIQ5ViewController.h
@@ -9,5 +9,57 @@
 #import <UIKit/UIKit.h>
 
 @interface KidsIQ5ViewController : UIViewController
+{
+    IBOutlet UILabel *question;
+    IBOutlet UILabel *answerA;
+    IBOutlet UILabel *answerB;
+    IBOutlet UILabel *answerC;
+    IBOutlet UILabel *answerD;
+    IBOutlet UILabel *myCounterLabel;
+    IBOutlet UIButton *choicea;
+    IBOutlet UIButton *choiceb;
+    IBOutlet UIButton *choicec;
+    IBOutlet UIButton *choiced;
+    IBOutlet UIButton *submit;
+    IBOutlet UILabel *score;
+    IBOutlet UILabel *nameLabel;
+    IBOutlet UILabel *result;
+    IBOutlet UIButton *test;
+    NSMutableData *responseData;
+}
 
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *country;
+@property (nonatomic, retain) NSMutableData *responseData;
+@property (nonatomic, retain) NSMutableSet *usedNumbers;
+@property (nonatomic, retain) UILabel *myCounterLabel;
+@property int maxQuestions;
+@property (assign) int level;
+@property (nonatomic, retain) NSTimer *mainTimer;
+
+-(IBAction)showQuitController;
+
+-(IBAction)showLoginViewController;
+
+-(IBAction)submit:(id)sender;
+
+-(void)resetAllChoices;
+
+-(void)resetAll;
+
+- (void)disableAllChoices;
+
+-(IBAction)checkAnswer;
+
+-(IBAction)skipQuestion;
+
+-(void)calculatescore;
+
+-(void)showResults;
+
+-(int)generateRandomNumber;
+
+-(IBAction)dismissView;
+
+-(IBAction)startCountdown:(id)sender;
 @end
