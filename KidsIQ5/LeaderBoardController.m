c6eb7064055fe7119eeb356251fc6f37e06325f4
Chan Komagan <chan@komagan.com>
2012-10-19 16:07:36 -0400

    Fixed pagination issues (previous and next). Added support for fact, math and science counts for details page

--- KidsIQ5/LeaderBoardController.m
+++ KidsIQ5/LeaderBoardController.m
@@ -9,6 +9,7 @@
 #import "LeaderBoardController.h"
 #import "NameViewController.h"
 #import "ASIFormDataRequest.h"
+#import "LeaderBoardDetailController.h"
 
 @interface LeaderBoardController()
 @property (nonatomic, strong) NSString *nsURL;
@@ -21,12 +22,13 @@ NSIndexPath *currentSelection;
 @synthesize responseData;
 NSDictionary *res;
 NSString *name, *score, *country;
+int fCount, mCount, sCount, fTCount, mTCount, sTCount;
 NSMutableArray *leaders;
-int leadercount = 0;
+int row, page = 0, totalItems;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithNibName:@"LeaderBoardController.xib" bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
     }
@@ -43,13 +45,20 @@ int leadercount = 0;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    page = 1;
     moreLeaders.hidden = TRUE;
     prevLeaders.hidden = TRUE;
     leaders = [[NSMutableArray alloc] init];
     copyNameList = nameList = [[NSMutableArray alloc] init];
     copyCountryList = countryList = [[NSMutableArray alloc] init];
     copyScoreList = scoreList = [[NSMutableArray alloc] init];
-    
+    copyfCountList = fCountList = [[NSMutableArray alloc] init];
+    copyfTCountList = fTCountList = [[NSMutableArray alloc] init];
+    copymCountList = mCountList = [[NSMutableArray alloc] init];
+    copymTCountList = mTCountList = [[NSMutableArray alloc] init];
+    copysCountList = sCountList = [[NSMutableArray alloc] init];
+    copysTCountList = sTCountList = [[NSMutableArray alloc] init];
+
     leaderList.scrollEnabled = YES;
         
     [leaderList setDelegate:self];
@@ -59,7 +68,7 @@ int leadercount = 0;
 
 -(void)receiveData
 {
-    nsURL = @"http://www.komagan.com/KidsIQ/leaders.php?format=json&getleaders=1";
+    nsURL = @"http://www.komagan.com/KidsIQ/leaders.php?format=json&getleaders2=1";
     self.responseData = [NSMutableData data];
     NSURLRequest *aRequest = [NSURLRequest requestWithURL:[NSURL URLWithString: nsURL]];
     [[NSURLConnection alloc] initWithRequest:aRequest delegate:self];
@@ -80,18 +89,24 @@ int leadercount = 0;
         name = [res1 objectForKey:@"name"];        
         score = [res1 objectForKey:@"score"];
         country = [res1 objectForKey:@"country"];
-        
         NSString *space = @"       ";
         NSString *row = [name stringByAppendingString:[space stringByAppendingString:[country stringByAppendingString:[space stringByAppendingString:score]]]];
-        //NSLog(@"%@", row);
+        //NSLog(@"%@", [res1 objectForKey:@"fTCount"]);
         [nameList addObject:name];
         [countryList addObject:country];
         [scoreList addObject:score];
+        [fCountList addObject:[res1 objectForKey:@"fCount"]];
+        [fTCountList addObject:[res1 objectForKey:@"fTCount"]];
+        [mCountList addObject:[res1 objectForKey:@"mCount"]];
+        [mTCountList addObject:[res1 objectForKey:@"mTCount"]];
+        [sCountList addObject:[res1 objectForKey:@"sCount"]];
+        [sTCountList addObject:[res1 objectForKey:@"sTCount"]];
         [leaders addObject:row];
+        totalItems++;
     }
     //[leaderList endUpdates];
     [leaderList reloadData];
-    
+    if (totalItems > 0) [self reset];
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
@@ -112,22 +127,55 @@ int leadercount = 0;
     [self dismissModalViewControllerAnimated:YES];
 }
 
--(IBAction)showMoreLeaders {
+- (void) reset {
     
-    nameList = [NSArray arrayWithArray:[copyNameList subarrayWithRange:NSMakeRange(7, [copyNameList count]-7)]];
-    scoreList = [NSArray arrayWithArray:[copyScoreList subarrayWithRange:NSMakeRange(7, [copyScoreList count]-7)]];
-    countryList = [NSArray arrayWithArray:[copyCountryList subarrayWithRange:NSMakeRange(7, [copyCountryList count]-7)]];
+    NSLog(@"we are on page = %d", page);
+
+    if(page*6 > (totalItems-1))
+    {
+        moreLeaders.hidden = TRUE;
+        page--;
+        if(page < 0) page = 0;
+    }
+    else{
+        moreLeaders.hidden = FALSE;
+    }
+}
+
+-(IBAction)showMoreLeaders {
+   
+   // NSLog(@"page count in more = %d", page);
+    nameList = [NSArray arrayWithArray:[copyNameList subarrayWithRange:NSMakeRange(page*6, [copysCountList count]-page*6)]];
+    scoreList = [NSArray arrayWithArray:[copyScoreList subarrayWithRange:NSMakeRange(page*6, [copysCountList count]-page*6)]];
+    countryList = [NSArray arrayWithArray:[copyCountryList subarrayWithRange:NSMakeRange(page*6, [copysCountList count]-page*6)]];
+    fCountList = [NSArray arrayWithArray:[copyfCountList subarrayWithRange:NSMakeRange(page*6, [copysCountList count]-page*6)]];
+    mCountList = [NSArray arrayWithArray:[copymCountList subarrayWithRange:NSMakeRange(page*6, [copysCountList count]-page*6)]];
+    sCountList = [NSArray arrayWithArray:[copysCountList subarrayWithRange:NSMakeRange(page*6, [copysCountList count]-page*6)]];
     prevLeaders.hidden = FALSE;
+    page++;
+    [self reset];
     [leaderList reloadData];
 }
 
 -(IBAction)showPreviousLeaders {
     
-    nameList = [NSArray arrayWithArray:[copyNameList subarrayWithRange:NSMakeRange(0, 6)]];
-    scoreList = [NSArray arrayWithArray:[copyScoreList subarrayWithRange:NSMakeRange(0, 6)]];
-    countryList = [NSArray arrayWithArray:[copyCountryList subarrayWithRange:NSMakeRange(0, 6)]];
-    prevLeaders.hidden = TRUE;
+    [self reset];
+    page--;
+
+    //x NSLog(@"page count in prev = %d", page);
+    nameList = [NSArray arrayWithArray:[copyNameList subarrayWithRange:NSMakeRange(page*6, 6)]];
+    scoreList = [NSArray arrayWithArray:[copyScoreList subarrayWithRange:NSMakeRange(page*6, 6)]];
+    countryList = [NSArray arrayWithArray:[copyCountryList subarrayWithRange:NSMakeRange(page*6, 6)]];
+    fCountList = [NSArray arrayWithArray:[copyfCountList subarrayWithRange:NSMakeRange(page*6, 6)]];
+    mCountList = [NSArray arrayWithArray:[copymCountList subarrayWithRange:NSMakeRange(page*6, 6)]];
+    sCountList = [NSArray arrayWithArray:[copysCountList subarrayWithRange:NSMakeRange(page*6, 6)]];
     [leaderList reloadData];
+    if(page <= 0)
+    {
+        moreLeaders.hidden = FALSE;
+        prevLeaders.hidden = TRUE;
+        page++;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -135,7 +183,7 @@ int leadercount = 0;
     static NSString *simpleTableIdentifier = @"SimpleTableItem";
     
     cell = [leaderList dequeueReusableCellWithIdentifier:simpleTableIdentifier];
-    
+
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
     }
@@ -166,12 +214,6 @@ int leadercount = 0;
     cellLabelS3.font = [UIFont systemFontOfSize: 15.0];
     [cell addSubview:cellLabelS3];
     
-    
-    //leaderList.backgroundColor = [UIColor redColor];
-    //leaderList.separatorColor = [UIColor clearColor];
-
-    //[cell.imageView setImage:img];
-    
     return cell;
 }
 
@@ -179,7 +221,6 @@ int leadercount = 0;
 {
     if([nameList count] >= 6)
     {
-        moreLeaders.hidden = FALSE;
         return 6;
     }
     else {
@@ -188,18 +229,49 @@ int leadercount = 0;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    
     return 1;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    currentSelection = indexPath;
-    //[self performSegueWithIdentifier:@"offerDetailsPage" sender:self];
+    NSUInteger row = [indexPath row];
+    name = [nameList objectAtIndex:row];
+    score = [scoreList objectAtIndex:row];
+    fCount = [[fCountList objectAtIndex:row] intValue];
+    fTCount = [[fTCountList objectAtIndex:row] intValue];
+    mCount = [[mCountList objectAtIndex:row] intValue];
+    mTCount = [[mTCountList objectAtIndex:row] intValue];
+    sCount = [[sCountList objectAtIndex:row] intValue];
+    sTCount = [[sTCountList objectAtIndex:row] intValue];
+    
+    [self performSegueWithIdentifier:@"showLeaderBoard" sender:self];
+}
+
+-(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showLeaderBoard"]) {
+        LeaderBoardDetailController *leaderDetailView = segue.destinationViewController;
+        leaderDetailView.name = name;
+        leaderDetailView.scoreDetail = score;
+        leaderDetailView.fCount = fCount;
+        leaderDetailView.mCount = mCount;
+        leaderDetailView.sCount = sCount;
+        leaderDetailView.fTCount = fTCount;
+        leaderDetailView.mTCount = mTCount;
+        leaderDetailView.sTCount = sTCount;
+        //NSString *scoresub = [score substringToIndex:[score length] - 1];
+        //leaderDetailView.scoreDetail = [scoresub floatValue];
+    }
 }
 
 -(IBAction)loginScreen {
     UIViewController *vc = [[UIStoryboard storyboardWithName:@"MainStoryboard_iPhone" bundle:nil]  instantiateViewControllerWithIdentifier:@"NameViewController"];
     [self presentModalViewController:vc animated:false];
+}
+
+-(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)orientation
+{
+    return (orientation != UIDeviceOrientationLandscapeLeft) &&
+	(orientation != UIDeviceOrientationLandscapeRight);
 }
 
 @end
